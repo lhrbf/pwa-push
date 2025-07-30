074a69e09fe7173cf82f3a4a319c03e2f155dc9f
Henrique <henrique_fraga@octor.pro>
2025-07-30 11:45:43 -0300

    Refactor layout structure and update global styles; set primary background color and ensure proper container height

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,14 +3,17 @@ import "../styles/main.css";
 import Header from "../components/Header";
 import Aside from "../components/Aside";
 import Footer from "../components/Footer";
+import "../styles/dashboard/styles.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
       <main>
-        <Aside />
-        <div className="container">{children}</div>
+        <div className="layout">
+          <Aside />
+          <div className="container">{children}</div>
+        </div>
       </main>
       <Footer />
     </>
